da31815871b086800fcfba5fd416b9119b109622
hemagso <hemagso@gmail.com>
2017-06-21 22:28:37 -0300

    Stopped midway through lesson 3

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -10,3 +11,11 @@ def index():
         {'author' : {'nickname' : 'Susan'}, 'body' : 'The avengers movie was so cool!'}
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID={1}, remember_me={2}'.format(form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
